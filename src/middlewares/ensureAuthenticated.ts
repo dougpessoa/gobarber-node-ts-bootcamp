a7172535cef485cbbff3f1910f7f54735c86bb7a
Douglas Pessoa <douglaspereira1@outlook.com>
2020-07-12 18:06:11 -0300

    primeira parte finalizada

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
-
+import AppError from '../errors/AppError';
 import authConfig  from '../config/auth';
 
 interface TokenPayload {
@@ -18,7 +18,7 @@ export default function ensureAuthenticated(
   const authHeader = request.headers.authorization;
 
   if(!authHeader) {
-    throw new Error('JWT token is missing');
+    throw new AppError('JWT token is missing', 401);
   }
 
   const [, token] = authHeader.split(' ');
@@ -29,12 +29,12 @@ export default function ensureAuthenticated(
     const { sub } = decoded as TokenPayload;
 
     request.user = {
-      id: sub,
-    };
+      id: sub
+    }
 
     return next();
   } catch {
-    throw new Error('Invalid JWT token');
+    throw new AppError('Invalid JWT token', 401);
   }
 
 }
